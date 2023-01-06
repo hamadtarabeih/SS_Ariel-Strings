e3092763d7244e8d5f49214c87c0c8061a34ead7
hamadtarabeih <118848249+hamadtarabeih@users.noreply.github.com>
2023-01-06 19:21:54 +0200

    Add files via upload

---
+++ myEX.h
@@ -0,0 +1,7 @@
+#define LINE 256
+#define MAXNUMOFLINES 250
+
+void shift_element(int* arr , int i);
+void insertion_sort(int * arr , int len);
+void txtserach(char text[MAXNUMOFLINES][LINE] , char string[]);
+void txtserach1(char text[MAXNUMOFLINES][LINE] , char string[]);
