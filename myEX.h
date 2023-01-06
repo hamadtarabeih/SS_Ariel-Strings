87fb35cfd160d86a2ff6cd854518216151b29065
hamadtarabeih <118848249+hamadtarabeih@users.noreply.github.com>
2023-01-06 19:56:39 +0200

    Delete myEX.h

--- myEX.h
+++
@@ -1,7 +0,0 @@
-#define LINE 256
-#define MAXNUMOFLINES 250
-
-void shift_element(int* arr , int i);
-void insertion_sort(int * arr , int len);
-void txtserach(char text[MAXNUMOFLINES][LINE] , char string[]);
-void txtserach1(char text[MAXNUMOFLINES][LINE] , char string[]);
