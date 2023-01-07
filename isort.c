8e1ea0fd844a660e3567bf31d1a95f5eea29e5d6
hamadtarabeih <118848249+hamadtarabeih@users.noreply.github.com>
2023-01-07 11:23:24 +0200

    Update isort.c

--- isort.c
+++ isort.c
@@ -2,22 +2,29 @@
 
 
 void shift_element(int* arr, int i) {
-    for (int j = i; j > 0; --j)
-        *(arr + j) = *(arr + j - 1);
+    for (int item = i; item > 0; --item)
+        *(arr + item) = *(arr + item - 1);
 }
 
-
+/*
+ * Function:  insertion_sort
+ * --------------------
+ * Makes an insertion sort to an array.
+ *
+ *  arr: starting pointer address of the array.
+ *
+ *  len: the array's size.
+ */
 void insertion_sort(int* arr, int len) {
     for (int i = 1; i < len; ++i)
     {
-        int Save = *(arr + i);
-        int j = (i - 1);
+        int key = *(arr + i), j = (i - 1);
 
-        while (j >= 0 && *(arr + j) > Save)
+        while (j >= 0 && *(arr + j) > key)
             --j;
 
         shift_element((arr + j), (i - j));
 
-        *(arr + j + 1) = Save;
+        *(arr + j + 1) = key;
     }
 }
